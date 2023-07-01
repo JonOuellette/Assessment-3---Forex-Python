923d82360c5923ef428da86870f14e5587971970
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-07-01 11:55:29 -0700

    fixed errors - caused by inconsistent naming.conversion or errors not displaying

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def currency_converter():
     """Checks for POST request - if it is a POST request it then retrieves the user values for currency codes and amounts"""
     if request.method == 'POST':
         from_currency = request.form['from_currency']
-        to_currency = request.form['to currency']
+        to_currency = request.form['to_currency']
         amount = request.form['amount']
     
         """Verifies if the currency codes entered are valid"""
-        if not converter.is_valid_currency(from_currency) or not converter.is_valid_currency(to_currency):
+        if not converter.valid_currency_check(from_currency) or not converter.valid_currency_check(to_currency):
             error_msg = "Invalid currency code."
             return render_template('home.html', error = error_msg)
 
