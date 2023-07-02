d9adce492e5da81cf803866ba3779a9296e20a38
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-07-01 23:05:38 -0700

    updated testing file

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 from app import app
+from converter import CurrencyConverter
+from exchange_rate_api import ExchangeRateAPI
 
 class AppTestCase(unittest.TestCase):
 
@@ -7,14 +9,16 @@ class AppTestCase(unittest.TestCase):
         self.app = app.test_client()
 
     def test_home_route(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Currency Converter', response.data)
+        with self.app as client:
+            response = self.app.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Currency Converter', response.data)
 
     def test_conversion_route(self):
-        response = self.app.post('/', data={'amount': 1, 'from_currency': 'USD', 'to_currency': 'USD'})
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Converted Amount:', response.data)
+        with self.app as client:
+            response = self.app.post('/', data={'amount': 1, 'from_currency': 'USD', 'to_currency': 'USD'})
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Converted Amount:', response.data)
 
 
 class ConversionTestCase(unittest.TestCase):
@@ -23,14 +27,15 @@ class ConversionTestCase(unittest.TestCase):
         self.app = app.test_client()
 
     def test_conversion_calculation(self):
-        response = self.app.post('/', data={'amount': 100, 'from_currency': 'USD', 'to_currency': 'EUR'})
-        self.assertEqual(response.status_code, 200)
+        with self.app as client:
+            response = self.app.post('/', data={'amount': 100, 'from_currency': 'USD', 'to_currency': 'EUR'})
+            self.assertEqual(response.status_code, 200)
 
-        # Assuming a known conversion rate of 1 USD = 0.85 EUR
-        expected_conversion = 100 * 0.85
+            # Assuming a known conversion rate of 1 USD = 0.85 EUR
+            expected_conversion = 100 * 0.85
 
-        # Extracting the converted amount from the response HTML
-        converted_amount = float(response.data.decode('utf-8').split('Converted Amount: ')[1])
+            # Extracting the converted amount from the response HTML
+            converted_amount = float(response.data.decode('utf-8').split('Converted Amount: ')[1])
 
-        # Asserting that the calculated converted amount matches the expected conversion
-        self.assertAlmostEqual(converted_amount, expected_conversion, places=2)
+            # Asserting that the calculated converted amount matches the expected conversion
+            self.assertAlmostEqual(converted_amount, expected_conversion, places=2)
